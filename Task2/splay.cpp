dd12f495a5fda4e8cf75485764ef4903bac74372
Smaran Pokharel <notsmaran@gmail.com>
2026-02-24 01:55:52 -0600

    updated task 3

---
+++ Task2/splay.cpp
@@ -0,0 +1,332 @@
+#include "splay.h"
+
+// default constructor
+SplayTree::SplayTree()
+{
+    root = nullptr;
+}
+
+// aka Zig (Right rotation around x)
+SplayTree::Node* SplayTree::rotateRight(Node* x)
+{
+    if (x == nullptr) return x;
+
+    Node* y = x->left;
+    if (y == nullptr) return x;
+
+    // Move y's right subtree to x's left
+    x->left = y->right;
+    if (y->right != nullptr)
+        y->right->parent = x;
+
+    // Link y to x's parent
+    y->parent = x->parent;
+
+    if (x->parent != nullptr) {
+        if (x->parent->left == x) x->parent->left = y;
+        else x->parent->right = y;
+    }
+
+    // Put x on y's right
+    y->right = x;
+    x->parent = y;
+
+    return y;
+}
+
+// aka Zag (Left rotation around x)
+SplayTree::Node* SplayTree::rotateLeft(Node* x)
+{
+    if (x == nullptr) return x;
+
+    Node* y = x->right;
+    if (y == nullptr) return x;
+
+    // Move y's left subtree to x's right
+    x->right = y->left;
+    if (y->left != nullptr)
+        y->left->parent = x;
+
+    // Link y to x's parent
+    y->parent = x->parent;
+
+    if (x->parent != nullptr) {
+        if (x->parent->left == x) x->parent->left = y;
+        else x->parent->right = y;
+    }
+
+    // Put x on y's left
+    y->left = x;
+    x->parent = y;
+
+    return y;
+}
+
+
+SplayTree::Node* SplayTree::splay(Node* root, int key)
+{
+    if (root == nullptr) return nullptr;
+
+    Node header(0);
+    header.left = nullptr;
+    header.right = nullptr;
+    header.parent = nullptr;
+
+    Node* leftTreeMax = &header;
+    Node* rightTreeMin = &header;
+
+    // Make sure current root is treated as a real root (detached)
+    root->parent = nullptr;
+
+    while (true) {
+
+        if (key < root->key) {
+
+            if (root->left == nullptr) break;
+
+            if (key < root->left->key) {
+                root = rotateRight(root);
+                root->parent = nullptr;
+                if (root->left == nullptr) break;
+            }
+
+            rightTreeMin->left = root;
+            if (rightTreeMin != &header) root->parent = rightTreeMin;
+            rightTreeMin = root;
+
+            root = root->left;
+            root->parent = nullptr;
+            rightTreeMin->left = nullptr;
+        }
+        else if (key > root->key) {
+
+            if (root->right == nullptr) break;
+
+            if (key > root->right->key) {
+                root = rotateLeft(root);
+                root->parent = nullptr;
+                if (root->right == nullptr) break;
+            }
+
+            leftTreeMax->right = root;
+            if (leftTreeMax != &header) root->parent = leftTreeMax;
+            leftTreeMax = root;
+
+            root = root->right;
+            root->parent = nullptr;
+            leftTreeMax->right = nullptr;
+        }
+        else {
+            break;
+        }
+    }
+
+    leftTreeMax->right = root->left;
+    if (leftTreeMax->right != nullptr)
+        leftTreeMax->right->parent = leftTreeMax;
+
+    rightTreeMin->left = root->right;
+    if (rightTreeMin->left != nullptr)
+        rightTreeMin->left->parent = rightTreeMin;
+
+    root->left = header.right;
+    if (root->left != nullptr)
+        root->left->parent = root;
+
+    root->right = header.left;
+    if (root->right != nullptr)
+        root->right->parent = root;
+
+    root->parent = nullptr;
+    return root;
+}
+
+SplayTree::Node* SplayTree::semiSplay(Node* root, int key, int k)
+{
+    if (root == nullptr) return nullptr;
+
+    Node header(0);
+    header.left = nullptr;
+    header.right = nullptr;
+    header.parent = nullptr;
+
+    Node* leftTreeMax = &header;
+    Node* rightTreeMin = &header;
+
+    // Make sure current root is treated as a real root (detached)
+    root->parent = nullptr;
+
+    int rotations = 0;
+
+    while (true) {
+
+        if (key < root->key) {
+
+            if (root->left == nullptr) break;
+
+            if (key < root->left->key && rotations < k) {
+                root = rotateRight(root);
+                root->parent = nullptr;
+                rotations++;
+                if (root->left == nullptr) break;
+            }
+
+            rightTreeMin->left = root;
+            if (rightTreeMin != &header) root->parent = rightTreeMin;
+            rightTreeMin = root;
+
+            root = root->left;
+            root->parent = nullptr;
+            rightTreeMin->left = nullptr;
+        }
+        else if (key > root->key) {
+
+            if (root->right == nullptr) break;
+
+            if (key > root->right->key && rotations < k) {
+                root = rotateLeft(root);
+                root->parent = nullptr;
+                rotations++;
+                if (root->right == nullptr) break;
+            }
+
+            leftTreeMax->right = root;
+            if (leftTreeMax != &header) root->parent = leftTreeMax;
+            leftTreeMax = root;
+
+            root = root->right;
+            root->parent = nullptr;
+            leftTreeMax->right = nullptr;
+        }
+        else {
+            break;
+        }
+    }
+
+    leftTreeMax->right = root->left;
+    if (leftTreeMax->right != nullptr)
+        leftTreeMax->right->parent = leftTreeMax;
+
+    rightTreeMin->left = root->right;
+    if (rightTreeMin->left != nullptr)
+        rightTreeMin->left->parent = rightTreeMin;
+
+    root->left = header.right;
+    if (root->left != nullptr)
+        root->left->parent = root;
+
+    root->right = header.left;
+    if (root->right != nullptr)
+        root->right->parent = root;
+
+    root->parent = nullptr;
+    return root;
+}
+
+SplayTree::Node* SplayTree::insertNode(Node* root, int key)
+{
+    // Normal BST insert, track parent pointers
+    if (root == nullptr) {
+        return new Node(key);
+    }
+
+    Node* cur = root;
+    Node* prev = nullptr;
+
+    while (cur != nullptr) {
+        prev = cur;
+        if (key < cur->key) cur = cur->left;
+        else if (key > cur->key) cur = cur->right;
+        else {
+            // Key already exists -> splay it anyway
+            return splay(root, key);
+        }
+    }
+
+    Node* newNode = new Node(key);
+    newNode->parent = prev;
+
+    if (key < prev->key) prev->left = newNode;
+    else prev->right = newNode;
+
+    // Splay inserted key to root
+    return splay(root, key);
+}
+
+SplayTree::Node* SplayTree::deleteNode(Node* root, int key)
+{
+    if (root == nullptr) return nullptr;
+
+    // Bring key (or closest) to root
+    root = splay(root, key);
+
+    // If key not found, no deletion
+    if (root == nullptr || root->key != key) return root;
+
+    Node* leftSub = root->left;
+    Node* rightSub = root->right;
+
+    if (leftSub != nullptr) leftSub->parent = nullptr;
+    if (rightSub != nullptr) rightSub->parent = nullptr;
+
+    delete root;
+
+    // If no left subtree, right becomes new root
+    if (leftSub == nullptr) {
+        return rightSub;
+    }
+
+    // Find maximum node in left subtree
+    Node* m = leftSub;
+    while (m->right != nullptr) m = m->right;
+
+    // Splay that maximum to root of left subtree
+    leftSub = splay(leftSub, m->key);
+
+    // Attach right subtree
+    leftSub->right = rightSub;
+    if (rightSub != nullptr) rightSub->parent = leftSub;
+
+    return leftSub;
+}
+
+void SplayTree::insert(int key)
+{
+    root = insertNode(root, key);
+}
+
+void SplayTree::remove(int key)
+{
+    root = deleteNode(root, key);
+}
+
+bool SplayTree::search(int key)
+{
+    root = semiSplay(root, key, 2);
+    return (root != nullptr && root->key == key);
+}
+
+void SplayTree::printTree(Node* root, int space)
+{
+    const int COUNT = 10;
+
+    if (root == nullptr)
+        return;
+
+    space += COUNT;
+
+    printTree(root->right, space);
+
+    for (int i = COUNT; i < space; i++)
+        cout << " ";
+
+    cout << root->key << endl;
+
+    printTree(root->left, space);
+}
+
+void SplayTree::display()
+{
+    printTree(root, 0);
+    cout << endl;
+}
---
+++ Task2/splay.h
@@ -0,0 +1,46 @@
+#ifndef SPLAYTREEE_H
+#define SPLAYTREEE_H
+
+#include <iostream>
+using namespace std;
+
+class SplayTree {
+
+    public:
+        struct Node {
+            int key;
+            Node* left;
+            Node* right;
+            Node* parent;
+
+            Node(int k) {
+                key = k;
+                left = nullptr;
+                right = nullptr;
+                parent = nullptr;
+            }
+        };
+
+    private:
+        Node* root;
+
+        Node* rotateRight(Node* x);
+        Node* rotateLeft(Node* x);
+
+        Node* splay(Node* root, int key);
+        Node* semiSplay(Node* root, int key, int k);
+
+        Node* insertNode(Node* root, int key);
+        Node* deleteNode(Node* root, int key);
+
+        void printTree(Node* root, int space);
+
+    public:
+        SplayTree();
+        void insert(int key);
+        void remove(int key);
+        bool search(int key);
+        void display();
+};
+
+#endif
